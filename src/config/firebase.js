9425300628fa82367b6bdf4752f52745f141cd6b
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-19 21:44:59 +0300

    connect to db - get all teachers

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getDatabase, ref, onValue } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -9,9 +10,20 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
 export const user = auth.currentUser;
+const database = getDatabase(app);
+
+console.log(database);
+
+const teachers = ref(database, "/");
+
+onValue(teachers, (snapshot) => {
+  const data = snapshot.val();
+  console.log(data);
+});
