0f506e1fc86a99526a5f2fe9275ba677ec9ad838
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-27 19:03:25 +0300

    adjust slect container flex-direction

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,15 +1,15 @@
-import { VStack } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
 import LanguageFilter from "./LanguageFilter";
 import PricesFilter from "./PricesFilter";
 import LevelsFilter from "./LevelsFilter";
 
 const Filters = () => {
   return (
-    <VStack spacing="20px" wrap="wrap" mb="32px">
+    <HStack spacing="20px" wrap="wrap" mb="32px">
       <LanguageFilter />
       <LevelsFilter />
       <PricesFilter />
-    </VStack>
+    </HStack>
   );
 };
 
