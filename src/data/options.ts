a19776f7ce98dd21d22391e24bf74e48d4d4b140
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-10-30 16:39:58 +0300

    fix not searchable upper-intermediate level

--- src/data/options.ts
+++ src/data/options.ts
@@ -1,4 +1,3 @@
-
 export type Option = {
   value: string;
   label: string;
@@ -10,7 +9,7 @@ export const levelsOptions: Options = [
   { value: "A1 Beginner", label: "A1 Beginner" },
   { value: "A2 Elementary", label: "A2 Elementary" },
   { value: "B1 Intermediate", label: "B1 Intermediate" },
-  { value: "B2 Upper Intermediate", label: "B2 Upper Intermediate" },
+  { value: "B2 Upper-Intermediate", label: "B2 Upper Intermediate" },
   { value: "C1 Advanced", label: "C1 Advanced" },
   { value: "C2 Proficient", label: "C2 Proficient" },
 ];
@@ -21,4 +20,3 @@ export const pricesOptions: Options = [
   { value: "30", label: "30 $" },
   { value: "40", label: "40 $" },
 ];
-
