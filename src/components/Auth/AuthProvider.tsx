6d7fc877d53142bc1f5ae4c4b7a56e8c321f0210
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-01 18:14:05 +0300

    Clean auth provider component

--- src/components/Auth/AuthProvider.tsx
+++ src/components/Auth/AuthProvider.tsx
@@ -1,68 +1,13 @@
-// import { useEffect } from "react";
-// import { onAuthStateChanged } from "firebase/auth";
-// import { useAppDispatch } from "../../redux/hooks";
-// import { refreshUser } from "../../redux/auth/slice.js";
-// import { auth } from "../../config/firebase.js";
-// import { firestore } from "../../config/firebase.js";
-// import { doc, getDoc } from "firebase/firestore";
-// import { refreshFavorites } from "../../redux/favorites/slice.js";
-
-// type AuthProviderProps = {
-//   children: React.ReactNode;
-// };
-
-// const AuthProvider = ({ children }: AuthProviderProps) => {
-//   const dispatch = useAppDispatch();
-
-//   useEffect(() => {
-//     const unsubscribe = onAuthStateChanged(auth, (user) => {
-//       if (user) {
-//         console.log("Auth state changed");
-//         dispatch(
-//           refreshUser({
-//             name: user.displayName as string,
-//             email: user.email as string,
-//             userId: user.uid as string,
-//           })
-//         );
-
-//         const docRef = doc(firestore, "users", user.uid);
-//         getDoc(docRef).then((docSnap) => {
-//           if (docSnap.exists()) {
-//             if (docSnap.data().favorites.length > 0) {
-//               const newFavorites = docSnap.data().favorites;
-//               dispatch(
-//                 refreshFavorites({
-//                   favoriteTeachersIds: newFavorites,
-//                 })
-//               );
-//             }
-//           } else {
-//             console.log("No such document!");
-//           }
-//         });
-//       }
-//     });
-
-//     return () => unsubscribe();
-//   }, [dispatch]);
-
-//   return <>{children}</>;
-// };
-
-// export default AuthProvider;
-
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect  } from "react";
 import { onAuthStateChanged } from "firebase/auth";
+import { auth, firestore   } from "../../config/firebase.js";
+import { doc, getDoc } from "firebase/firestore";
+
 import { useAppDispatch } from "../../redux/hooks";
 import { refreshUser } from "../../redux/auth/slice.js";
-import { auth } from "../../config/firebase.js";
-import { firestore } from "../../config/firebase.js";
-import { doc, getDoc } from "firebase/firestore";
 import { refreshFavorites } from "../../redux/favorites/slice.js";
+
 import Loader from "../common/Loader.js";
-import { Box } from "@chakra-ui/react";
 
 type AuthProviderProps = {
   children: React.ReactNode;
@@ -101,7 +46,7 @@ const AuthProvider = ({ children }: AuthProviderProps) => {
 
   if (loading) return <Loader />;
 
-  return <Box minHeight="100%">{children}</Box>;
+  return <>{children}</>;
 };
 
 export default AuthProvider;
