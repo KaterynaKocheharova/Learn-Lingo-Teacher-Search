733185c5431cc907743f008a0d84d1a15a9cc3ec
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-01 18:08:00 +0300

    change toast position

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -1,12 +1,11 @@
-
 type ToastConfigs = {
   duration: number;
   isClosable: boolean;
+  position: string;
 };
 
 export const toastConfigs: ToastConfigs = {
   duration: 2000,
   isClosable: true,
+  position: "top-right",
 };
-
-
