696d818ee7eda16417f63d102f9755742c455938
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-01 21:02:34 +0300

    edit PageError component

--- src/components/common/PageError.tsx
+++ src/components/common/PageError.tsx
@@ -6,7 +6,7 @@ type PageErrorProps = {
 
 const PageError = ({ error }: PageErrorProps) => {
   return (
-    <Center pos="relative" pt="100px">
+    <Center pos="relative">
       <Text as="h2" color="red.400" fontSize="24px" pos="absolute">
         {error ? error : "Wooops. Something went wrong. Check out the internet connection or try again later"}
       </Text>
