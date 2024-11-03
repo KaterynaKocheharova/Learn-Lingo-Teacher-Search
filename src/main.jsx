6821c9b8243bc0d7533d003bc47085f40d51a97c
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-03 14:41:47 +0300

    remove commented code from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,24 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store, persistor } from "./redux/store.ts";
+import { store } from "./redux/store.ts";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./theme.ts";
 import "modern-normalize";
 import App from "./App.jsx";
 import "./index.css";
-import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <PersistGate loading={null} persistor={persistor}> */}
       <ChakraProvider theme={theme}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </ChakraProvider>
-      {/* </PersistGate> */}
     </Provider>
   </React.StrictMode>
 );
