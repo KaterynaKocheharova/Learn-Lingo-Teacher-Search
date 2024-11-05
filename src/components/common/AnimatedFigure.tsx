25dfd805372f0d6c493e11763955d08bd5cee4c9
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-06 01:39:16 +0300

    add gradient and animation to sections

---
+++ src/components/common/AnimatedFigure.tsx
@@ -0,0 +1,34 @@
+import { Box, BoxProps } from "@chakra-ui/react";
+import { keyframes } from "@emotion/react";
+
+type AninmatedFigureProps = BoxProps & {
+  animation: string;
+};
+
+const circlesAnimation = keyframes`
+  0% {
+    transform: scale(0) translate(0);
+    opacity: 1;
+  }
+  100% {
+      transform: scale(1.3) translate(-90px);
+    opacity: 1;
+  }
+`;
+
+const AnimatedFigure = ({ animation, ...props }: AninmatedFigureProps) => {
+  return (
+    <Box
+      pos="absolute"
+      w="30px"
+      h="30px"
+      borderRadius="60px"
+      bg="transparent"
+      border="3px solid #FFFFFF"
+      animation={`${circlesAnimation} ${animation}`}
+      {...props}
+    ></Box>
+  );
+};
+
+export default AnimatedFigure;
--- src/components/common/PageSection.tsx
+++ src/components/common/PageSection.tsx
@@ -1,12 +1,42 @@
 import { Box } from "@chakra-ui/react";
+import AnimatedFigure from "./AnimatedFigure";
 
-type PageSection = {
+type PageSectionProps = {
   children: React.ReactNode;
 };
 
-const PageSection = ({ children }: PageSection) => {
+const PageSection = ({ children }: PageSectionProps) => {
   return (
-    <Box bg="brand.gray.500" py="96px" as="section" minHeight="100vh">
+    <Box
+      backgroundImage="linear-gradient(90deg, var(--chakra-colors-brand-gray-500), var(--chakra-colors-brand-orange-100))"
+      py="96px"
+      as="section"
+      minHeight="100vh"
+      pos="relative"
+      w="100%"
+      h="100%"
+      overflow="hidden"
+    >
+      <AnimatedFigure top="5%" right="1%" animation="8s linear infinite" />
+      <AnimatedFigure top="10%" right="1%" animation=" 12s linear infinite" />
+      <AnimatedFigure top="15%" right="1%" animation=" 7s linear infinite" />
+      <AnimatedFigure top="20%" right="1%" animation=" 10s linear infinite" />
+      <AnimatedFigure top="25%" right="1%" animation=" 5s linear infinite" />
+      <AnimatedFigure top="30%" right="1%" animation=" 14s linear infinite" />
+      <AnimatedFigure top="35%" right="1%" animation=" 6s linear infinite" />
+      <AnimatedFigure top="40%" right="1%" animation=" 9s linear infinite" />
+      <AnimatedFigure top="45%" right="1%" animation=" 11s linear infinite" />
+      <AnimatedFigure top="50%" right="1%" animation=" 13s linear infinite" />
+      <AnimatedFigure top="55%" right="1%" animation=" 7s linear infinite" />
+      <AnimatedFigure top="60%" right="1%" animation=" 15s linear infinite" />
+      <AnimatedFigure top="65%" right="1%" animation=" 6s linear infinite" />
+      <AnimatedFigure top="70%" right="1%" animation=" 9s linear infinite" />
+      <AnimatedFigure top="75%" right="1%" animation=" 8s linear infinite" />
+      <AnimatedFigure top="80%" right="1%" animation=" 14s linear infinite" />
+      <AnimatedFigure top="85%" right="1%" animation=" 7s linear infinite" />
+      <AnimatedFigure top="90%" right="1%" animation=" 10s linear infinite" />
+      <AnimatedFigure top="95%" right="1%" animation=" 12s linear infinite" />
+
       {children}
     </Box>
   );
