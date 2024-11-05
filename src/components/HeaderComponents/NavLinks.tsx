904680384a7aaff58e1b2fc2c1b0be9e0a694c6a
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-05 22:48:17 +0300

    add active link styles

--- src/components/HeaderComponents/NavLinks.tsx
+++ src/components/HeaderComponents/NavLinks.tsx
@@ -2,20 +2,26 @@ import { useAppSelector } from "../../redux/hooks";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import { NavLink } from "react-router-dom";
 import { HStack, Text } from "@chakra-ui/react";
+import clsx from "clsx";
+import css from "./NavLinks.module.css";
 
 const NavLinks = () => {
   const isLoggedIn = useAppSelector(selectIsLoggedIn);
-  
+
+  const buildActiveClass = ({ isActive }: { isActive: boolean }) => {
+    return clsx(isActive && css["active-link"]);
+  };
+
   return (
     <HStack as="ul" spacing="7" wrap="wrap">
-      <NavLink to="/">
+      <NavLink to="/" className={buildActiveClass}>
         <Text py="8">Home</Text>
       </NavLink>
-      <NavLink to="/teachers">
+      <NavLink to="/teachers" className={buildActiveClass}>
         <Text py="8">Teachers</Text>
       </NavLink>
       {isLoggedIn && (
-        <NavLink to="/favorites">
+        <NavLink to="/favorites" className={buildActiveClass}>
           <Text py="8">Favorites</Text>
         </NavLink>
       )}
