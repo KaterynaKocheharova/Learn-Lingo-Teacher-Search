1117edc1fd31a74f6b848725adb452ea2fd6d2dc
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-07 22:42:29 +0300

    add toast when removing all circles

--- src/components/common/AnimationWrapper.tsx
+++ src/components/common/AnimationWrapper.tsx
@@ -1,58 +1,9 @@
-// import { useState } from "react";
-// import { nanoid } from "nanoid";
-// import { Box, useBreakpointValue } from "@chakra-ui/react";
-// import AnimatedFigure from "./AnimatedFigure";
-
-// type AnimationWrapperProps = {
-//   children: React.ReactNode;
-// };
-
-// const AnimationWrapper = ({ children }: AnimationWrapperProps) => {
-//   const showAnimatedFigures = useBreakpointValue({ base: false, lg: true });
-
-//   const [isClicked, setIsClicked] = useState<string[]>([]);
-
-//   const handleClicked = (e) => {
-//     setIsClicked((prev) => {
-//       return [...prev, e.currentTarget.id];
-//     });
-//   };
-
-//   const animatedFigures = Array.from({ length: 19 }, (_, index) => ({
-//     id: `figure-${nanoid()}`,
-//     top: `${5 * (index + 1)}%`,
-//     animation: `${5 + (index % 10)}s linear infinite`,
-//   }));
-
-//   return (
-//     <Box pos="relative">
-//       {showAnimatedFigures && (
-//         <>
-//           {animatedFigures.map((figure) => (
-//             <AnimatedFigure
-//               id={figure.id}
-//               onClick={handleClicked}
-//               display={isClicked.includes(figure.id) ? "none" : "block"}
-//               key={figure.id}
-//               top={figure.top}
-//               right="1%"
-//               animation={figure.animation}
-//             />
-//           ))}
-//         </>
-//       )}
-
-//       {children}
-//     </Box>
-//   );
-// };
-
-// export default AnimationWrapper;
-
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { nanoid } from "nanoid";
 import { Box, useBreakpointValue } from "@chakra-ui/react";
 import AnimatedFigure from "./AnimatedFigure";
+import { useToast } from "@chakra-ui/react";
+import { toastConfigs } from "../../config/toast";
 
 type AnimationWrapperProps = {
   children: React.ReactNode;
@@ -60,22 +11,43 @@ type AnimationWrapperProps = {
 
 const AnimationWrapper = ({ children }: AnimationWrapperProps) => {
   const showAnimatedFigures = useBreakpointValue({ base: false, lg: true });
+  const toast = useToast();
 
   const [isClicked, setIsClicked] = useState<string[]>([]);
-
-  const handleClicked = (id: string) => {
-    setIsClicked((prev) => [...prev, id]);
-  };
 
   const animatedFigures = useMemo(
     () =>
       Array.from({ length: 19 }, (_, index) => ({
         id: `figure-${nanoid()}`,
         top: `${5 * (index + 1)}%`,
-        animation: `${5 + index * 2}s linear infinite`,
+        animation: `${10 - index * 0.5}s linear infinite`,
       })),
     []
   );
+
+  const handleClicked = (id: string) => {
+    setIsClicked((prev) => [...prev, id]);
+  };
+
+  useEffect(() => {
+    if (isClicked.length === animatedFigures.length) {
+      toast({
+        ...toastConfigs,
+        description: "Congrats! You killed all circles",
+        status: "success",
+      });
+    }
+  }, [isClicked]);
+
+  //   const animatedFigures = useMemo(
+  //     () =>
+  //       Array.from({ length: 19 }, (_, index) => ({
+  //         id: `figure-${nanoid()}`,
+  //         top: `${5 * (index + 1)}%`,
+  //         animation: `${5 + index * 2}s linear infinite`,
+  //       })),
+  //     []
+  //   );
 
   return (
     <Box pos="relative">
