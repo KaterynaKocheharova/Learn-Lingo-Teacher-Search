d0632840dc9e0a17b5ff4cbed421b0594043af6e
KaterynaKocheharova <146726316+KaterynaKocheharova@users.noreply.github.com>
2024-11-08 22:30:31 +0300

    modify filter prices function - now array is returned with the value of label with appended $

--- src/utils/filterOptions.ts
+++ src/utils/filterOptions.ts
@@ -2,14 +2,21 @@ import { type Options } from "../data/options";
 
 type FilterOptions = (inputValue: string, options: Options) => Options;
 export const filterOptions: FilterOptions = (inputValue, options) => {
-  return options.filter((option) => {
-    if (typeof option.label === "string") {
-      return option.label.toLowerCase().includes(inputValue.toLowerCase());
-    } else if (typeof option.label === "number") {
-      return String(option.label)
-        .toLowerCase()
-        .includes(inputValue.toLowerCase());
-    }
-    return false;
-  });
+  return options
+    .filter((option) => {
+      if (typeof option.label === "string") {
+        return option.label.toLowerCase().includes(inputValue.toLowerCase());
+      } else if (typeof option.label === "number") {
+        return String(option.label)
+          .toLowerCase()
+          .includes(inputValue.toLowerCase());
+      }
+      return false;
+    })
+    .map((option) => {
+      return {
+        ...option,
+        label: `${option.label} $`,
+      };
+    });
 };
